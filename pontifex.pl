d786602d0d5f3eb81832e84e13607405a99f6da9
Alistair Mann <al+tfl@pectw.net>
2022-09-22 22:08:50 +0100

    Make readable with variable and subroutine name changes that seem accurate

--- pontifex.pl
+++ pontifex.pl
@@ -1,37 +1,40 @@
 #!/usr/bin/perl -s
-
-$f = $d ? -1 : 1;
-$D = pack('C*', 33..86);
-$p = shift;
-$p =~ y/a-z/A-Z/;
-$U = '$D =~ s/(.*)U$/U$1/; $D =~ s/U(.)/$1U/;';
-($V = $U) =~ s/U/V/g;
-$p =~ s/[A-Z]/$k = ord($&) - 64, &e/eg;
-$k = 0;
+# $decrypt_or_encrypt = 1;
+$encrypt_or_decrypt = $decrypt_or_encrypt ? -1 : 1;
+$deck_of_cards = pack('C*', 33..86);
+$password = shift;
+$password =~ y/a-z/A-Z/;
+$rotate_u_right = '$deck_of_cards =~ s/(.*)U$/U$1/; $deck_of_cards =~ s/U(.)/$1U/;';
+($rotate_v_right = $rotate_u_right) =~ s/U/V/g;
+$password =~ s/[A-Z]/$index_into_deck = ord($&) - 64, &run_the_engine/eg;
+$index_into_deck = 0;
 while(<>)
 {
     y/a-z/A-Z/;
     y/A-Z//dc;
-    $o .= $_;
+    $output .= $_;
 }
-$o .= 'X' while length($o) %5 && !$d;
-$o =~ s/./chr(($f * &e + ord($&) - 13) % 26 + 65)/eg;
-$o =~ s/X*$// if $d;
-$o =~ s/.{5}/$& /g;
-print("$o\n");
-sub v
+$output .= 'X' while length($output) %5 && !$decrypt_or_encrypt;
+$output =~ s/./chr(($encrypt_or_decrypt * &run_the_engine + ord($&) - 13) % 26 + 65)/eg;
+$output =~ s/X*$// if $decrypt_or_encrypt;
+$output =~ s/.{5}/$& /g;
+print("$output\n");
+sub get_nth_card_from_deck_cap53
 {
-    $v = ord(substr($D, $_[0]))-32;
+    $v = ord(substr($deck_of_cards, $_[0])) - 32;
     $v > 53 ? 53 : $v;
 }
-sub w
+sub rotate_deck_by_n
 {
-    $D =~ s/(.{$_[0]})(.*)(.)/$2$1$3/;
+    $deck_of_cards =~ s/(.{$_[0]})(.*)(.)/$2$1$3/;
 }
-sub e
+sub run_the_engine
 {
-    eval"$U$V$V";
-    $D =~ s/(.*)([UV].*[UV])(.*)/$3$2$1/;
-    &w(&v(53));
-    $k ? (&w($k)) : ($c = &v(&v(0)), $c > 52 ? &e : $c)
+    eval"$rotate_u_right$rotate_v_right$rotate_v_right";
+    $deck_of_cards =~ s/(.*)([UV].*[UV])(.*)/$3$2$1/;
+    &rotate_deck_by_n(&get_nth_card_from_deck_cap53(53));
+    $index_into_deck ?
+	(&rotate_deck_by_n($index_into_deck))
+	: ($character = &get_nth_card_from_deck_cap53(&get_nth_card_from_deck_cap53(0)),
+	   $character > 52 ? &run_the_engine : $character);
 }
